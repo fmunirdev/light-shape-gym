e4b4eae91908088bcec96ac89db54f4405b19ebd
Rejaur Rahman <rejaur-rahman@hotmail.co.ukm>
2021-07-03 16:26:58 +0100

    renamed gatsby structure so it runs in typescript file naming and added .dockerignore to frontend

--- frontend/gatsby-node.js
+++ frontend/gatsby-node.js
@@ -4,4 +4,17 @@
  * See: https://www.gatsbyjs.com/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const path = require('path')
+
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@layouts': path.resolve(__dirname, 'src/layouts'),
+        '@images': path.resolve(__dirname, 'src/images'),
+        '@fonts': path.resolve(__dirname, 'src/fonts')
+      },
+    },
+  })
+}
