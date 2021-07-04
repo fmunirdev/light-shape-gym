8a7209d35f1082a6e4a774e1b822b49e8e63bd5c
Rejaur Rahman <rejaur-rahman@hotmail.co.ukm>
2021-07-05 00:20:04 +0100

    setup sass structure on gatsby

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -29,6 +29,18 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        additionalData: "@use 'base' as *;",
+        sassOptions: {
+          includePaths: [`${__dirname}/src/styles`]
+        },
+        cssLoaderOptions: {
+          camelCase: false
+        }
+      }
+    },
     `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
