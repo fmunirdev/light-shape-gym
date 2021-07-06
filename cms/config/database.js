9e6f495fa95160a0b858d2768b7f89e2afe5c39c
Ronny Rahman <ronny.rahman@itech.media>
2021-07-06 20:17:05 +0100

    added Dockerfile to cms and updated structure to try and get strapi and mongo.db to connect

--- cms/config/database.js
+++ cms/config/database.js
@@ -2,13 +2,18 @@ module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: 'mongoose',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        host: env('DATABASE_HOST', 'gym_datastore'),
+        srv: env.bool('DATABASE_SRV', false),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'lsg_strapi_db'),
+        username: env('DATABASE_USERNAME', 'lsg_strapi_u01'),
+        password: env('DATABASE_PASSWORD', 'lsg_strapi_p01'),
       },
       options: {
-        useNullAsDefault: true,
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', false),
       },
     },
   },
